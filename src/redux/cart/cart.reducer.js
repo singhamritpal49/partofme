775d703866f91505dfdd2b856d21f3589dcfa341
Amritpal Singh <singhamritpal49@gmail.com>
2020-03-18 00:29:23 -0400

    arrow feature

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,13 +1,13 @@
 import CartActionTypes from './cart.types.js'
-import { addItemToCart }from './cart.utils.js'
+import { addItemToCart, removeItemFromCart } from './cart.utils.js'
 const INITIAL_STATE = {
     hidden: true,
     cartItems: []
 };
 
 
-const cartReducer = (state = INITIAL_STATE,action) => {
-    switch(action.type) {
+const cartReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
         case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
@@ -18,7 +18,20 @@ const cartReducer = (state = INITIAL_STATE,action) => {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
             }
-            default: 
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id)
+            }
+
+
+        default:
             return state;
     }
 }
