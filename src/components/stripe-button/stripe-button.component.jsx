4dd2bbf5e78048fff50fe8eea8b4217f0a15110a
Amritpal Singh <singhamritpal49@gmail.com>
2020-03-21 19:17:06 -0400

    updated IMAGE URL

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -17,7 +17,7 @@ const StripeCheckoutButton = ({price}) => {
         name='PARTOFME COMPANY & CO'
         billingAddress
         shippingAddress
-        image='https://uccd719e008a55808adef14b02da.previews.dropboxusercontent.com/p/thumb/AAvRumo7i1lasKnzqTuLXuOG8LsgpPJTsLkF6VwxUaapxk6TyM7wf14FdOXLCjS9c7gfs5xNT5BOCQV2Kz1CJ4rNzRjZEwR7EFy0NZdqs9SzYzOzCRzYkteVQMb44Z_qdXnijlxBgNXNLKYu3GTlLTIrZ511V9chJiXABMR09Jx1sGrgwVadzEEEkvxkAyIZJ1F78AxX-JofINCHK2zaj5XLHnWr2F-yGO19Sjh-Qbpe8v6pDbam45LV9e3k9DBkMbJ0G4DIBLfMVu2VyeGBZUQLHs1A-QrUWSMeP-qaJcez4p-uluMIPtsaRkUbZzMbtUHX7jNdrPMXfDbcFWICV7Tpr5Uh5syUs2rfjrxirIqUjy4OSQneVutH-hTZCNNNOWQey0qTrQSxgLOII7lYRfH6/p.png?fv_content=true&size_mode=5'
+        image='https://i.ibb.co/TgYr96k/business-suit.png'
         description={`Your total is $${price}`}
         amount={priceForStripe}
         panelLabel='Pay Now'
